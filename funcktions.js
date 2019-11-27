0301c8ccaaafacd800bd8640d515100cc146f71b
Pepe <jose.mateo@live.u-tad.com>
2019-11-27 08:59:48 +0100

    Falta poner fotos del mismo tamaño. Falta ajustar buenos colores a las animaciones de los botones.

--- funcktions.js
+++ funcktions.js
@@ -1,26 +1,31 @@
 
 var folderPhoto = ['Gatito0.jpg', 'Gatito1.jpg', 'Gatito2.jpg', 'Gatito3.jpg', 'Gatito4.jpg'];
 var c = 0;
-var photo = document.querySelector('#photo');
 
+var photo = document.querySelector('#photo');
 function changePhotoPlus(butt) {
     if (butt === 'Previous') {
         if (c != 0) {
             c--;
             photo.setAttribute("src", "gatitos/" + folderPhoto[c]);
+            console.log(c);
         } 
     } else if (c != folderPhoto.length - 1) {
         c++;
         photo.setAttribute("src", "gatitos/" + folderPhoto[c]);
+        console.log(c);
     } 
-}
-
-function validarBtn(c){
     if (c===0) {
         document.getElementById("Previous").disabled = true;
+    } else {
+        document.getElementById("Previous").disabled = false;
     }
-    if (c===folderPhoto.length - 1) {
+    if (c===4) {
         document.getElementById("Next").disabled = true;
+    } else {
+        document.getElementById("Next").disabled = false;
     }
 }
 
+
+    
